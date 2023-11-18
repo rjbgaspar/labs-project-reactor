bb4474ee28689687762d7d5334be4375a236b440
Ricardo Gaspar <47356635+rjbgaspar@users.noreply.github.com>
2023-11-18 11:39:02 +0000

    first commit

---
+++ src/test/java/com/example/projectreactor/ProjectReactorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectreactor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectReactorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
